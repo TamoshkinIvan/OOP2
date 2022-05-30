f9ea1fa11f6793540c5972d6a9158d9775affe6f
Ivan Tamoshkin <57822372+TamoshkinIvan@users.noreply.github.com>
2022-05-30 10:24:47 +0700

    test

---
+++ LB3/ClassLibrary3/GoodsType.cs
@@ -0,0 +1,12 @@
+﻿namespace ModelLab3
+{
+    /// <summary>
+    /// Список типов товара
+    /// </summary>
+    public enum GoodsType
+    {
+        Clothes,
+        Food,
+        ChildrenProducts
+    }
+}
